19a4e0bc38cc588d5cd33c6d59ee3cfad8e60a89
mateuslf <mateuslfvrb@hotmail.com>
2026-07-18 14:28:38 -0300

    Verifica colisão com terreno

--- Scenes/bullet.gd
+++ Scenes/bullet.gd
@@ -7,7 +7,6 @@ var direction = Vector2.RIGHT
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -17,3 +16,11 @@ func _physics_process(delta: float) -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+func _on_body_entered(body):
+	#Se for o player da dano 
+	if body.name == "Player":
+		queue_free()
+	#Se for terreno só some
+	else:
+		queue_free()
