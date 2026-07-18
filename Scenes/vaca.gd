36f82dae2173e70236254d3386dc713d8c7ad418
Lucas Araujo de Oliveira <lucasaraujo.oliveira@estudante.ufjf.br>
2026-07-18 12:23:49 -0300

    lenhador, funcao de dano, funcao de morrer

--- Scenes/vaca.gd
+++ Scenes/vaca.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
-
+var health := 3
+var invulnerable := false
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var dead = false
 
 func _physics_process(delta: float) -> void:
+	if dead:
+		return
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -23,3 +26,34 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func take_damage(amount: int, enemy_position: Vector2):
+	if invulnerable:
+		return
+	health -= amount
+	invulnerable = true
+	
+	#Empurrão para trás
+	velocity.y = -250
+	velocity.x = sign(global_position.x - enemy_position.x) * 200
+	
+	if health<=0:
+		die()
+		return
+	await get_tree().create_timer(1.0).timeout
+	invulnerable = false
+
+func die():
+	
+	dead = true
+	collision_layer = 0
+	collision_mask = 0
+	$CollisionShape2D.disabled = true
+	
+	velocity = Vector2.ZERO
+	
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0.0, 0.5)
+	await tween.finished
+	
+	queue_free()
