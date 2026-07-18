a06be694cb17ad0871bf8e4a3843501631245351
mateuslf <mateuslfvrb@hotmail.com>
2026-07-18 14:08:36 -0300

    Adiciona projétil

---
+++ Scenes/bullet.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var speed = 2.0
+var direction = Vector2.RIGHT
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	position += direction * speed * delta
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
