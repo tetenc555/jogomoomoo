867f40e9cf058538de8cdac30cd83a7f7181a046
Lucas Araujo de Oliveira <lucasaraujo.oliveira@estudante.ufjf.br>
2026-07-18 16:22:03 -0300

    ajusta as layers de player e lenhador para causar dano corretamente

--- Scenes/lenhador.gd
+++ Scenes/lenhador.gd
@@ -17,8 +17,6 @@ func _physics_process(delta: float) -> void:
 		turn()
 	
 	move_and_slide()
-	
-	check_player_collision()
 
 var direction := -1
 var dead := false
@@ -37,13 +35,6 @@ func _process(delta: float) -> void:
 @onready var floor_detector = $FloorDetector #Virar ao encontrar penhasco
 @export var damage = 1
 
-func check_player_collision():
-	for i in range (get_slide_collision_count()):
-		var collision = get_slide_collision(i)
-		var body = collision.get_collider()
-		
-		if body.is_in_group("Player"):
-			body.take_damage(damage, global_position)
 func turn():
 	direction *= -1
 	sprite.flip_h = direction < 0
@@ -75,3 +66,9 @@ func _on_head_body_entered(body: Node2D) -> void:
 		velocity = Vector2.ZERO
 		die()
 		body.velocity.y = -350
+
+
+func _on_hurt_box_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		print("encontrou")
+		body.take_damage(damage, global_position)
