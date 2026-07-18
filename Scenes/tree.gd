0520329bd4d19ff9f32752ccb2aa689413a16ebe
mateuslf <mateuslfvrb@hotmail.com>
2026-07-18 18:49:38 -0300

    Adiciona arvore que cai

---
+++ Scenes/tree.gd
@@ -0,0 +1,22 @@
+extends StaticBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func die() -> void:
+	var angulo_alvo = rotation + deg_to_rad(90)
+	
+	var tween = create_tween()
+	tween.tween_property(self, "rotation", angulo_alvo, 0.4).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	
+	tween.tween_property(self, "global_position:y", self.global_position.y + 13.0, 0.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+		
+func _input(event):
+	if event.is_action_pressed("ui_accept"): # Barra de Espaço / Enter por padrão
+		die()
