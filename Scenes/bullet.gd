a0b35cd3f368d2a1342f218b78d5c5cc57c03733
mateuslf <mateuslfvrb@hotmail.com>
2026-07-18 16:37:15 -0300

    adiciona colisão com parede

--- Scenes/bullet.gd
+++ Scenes/bullet.gd
@@ -1,18 +1,9 @@
 extends Area2D
 
-
 @onready var sprite = $Sprite2D
 var speed = 50.0
 var direction = Vector2.RIGHT
 var damage = 1
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position += direction * speed * delta
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
@@ -23,3 +14,9 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 func set_speed(direction : int) -> void:
 	speed *= -1
 	sprite.flip_h = true
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is TileMapLayer:
+		print("Bateu no cenário!")
+		queue_free()
